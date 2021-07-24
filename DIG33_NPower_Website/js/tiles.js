3ab2ab7ec061cfcc0b5703703511c6e3f18cfa0e
s3775633 <s3775633@student.rmit.edu.au>
2021-07-24 15:00:10 +1000

    Home page tiles
    Added imagery to home page tiles

--- DIG33_NPower_Website/js/tiles.js
+++ DIG33_NPower_Website/js/tiles.js
@@ -4,17 +4,21 @@ function mouseOver(div) {
     var image = div.querySelector(".image");
     var text = div.querySelector(".tileText");
     var heading = div.querySelector(".tileHeading");
+    var tileImage = div.querySelector(".cardImage");
     var description = div.querySelector(".tileDescription");
     image.classList.add("fade-out");
     text.classList.add("fade-out");
     heading.classList.remove("fade-out-description");
     description.classList.remove("fade-out-description");
+    tileImage.classList.remove("fade-out-description");
     image.classList.remove("fade-in");
     text.classList.remove("fade-in");
     heading.classList.add("fade-in-description");
     description.classList.add("fade-in-description");
     heading.classList.remove("tileDecriptionVisibility");
     description.classList.remove("tileDecriptionVisibility");
+    tileImage.classList.add("fade-in-description");
+    tileImage.classList.remove("tileDecriptionVisibility");
 }
 
 // function used to add and remove classes from home page tiles to give additional
@@ -24,12 +28,15 @@ function mouseOut(div) {
   var text = div.querySelector(".tileText");
   var heading = div.querySelector(".tileHeading");
   var description = div.querySelector(".tileDescription");
+  var tileImage = div.querySelector(".cardImage");
   image.classList.remove("fade-out");
   text.classList.remove("fade-out");
   heading.classList.add("fade-out-description");
   description.classList.add("fade-out-description");
+  tileImage.classList.add("fade-out-description");
   image.classList.add("fade-in");
   text.classList.add("fade-in");
   heading.classList.remove("fade-in-description");
   description.classList.remove("fade-in-description");
+  tileImage.classList.remove("fade-in-description");
 }
