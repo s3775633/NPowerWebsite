1bdc33fcee226151fa7502e441aab4d2d4047808
s3775633 <s3775633@student.rmit.edu.au>
2021-07-24 16:26:13 +1000

    Improvement page
    Created improvements page

---
+++ DIG33_NPower_Website/js/improvement.js
@@ -0,0 +1,60 @@
+// Code obtained from https://codepen.io/technokami/pen/abojmZa
+/* Store the element in el */
+let house = document.getElementById('house');
+
+/* Get the height and width of the element */
+const height = house.clientHeight
+const width = house.clientWidth
+
+/*
+  * Add a listener for mousemove event
+  * Which will trigger function 'handleMove'
+  * On mousemove
+  */
+house.addEventListener('mousemove', handleMove)
+
+console.log("Hello");
+/* Define function a */
+function handleMove(e) {
+  /*
+    * Get position of mouse cursor
+    * With respect to the element
+    * On mouseover
+    */
+  /* Store the x position */
+  const xVal = e.layerX
+  /* Store the y position */
+  const yVal = e.layerY
+  
+  /*
+    * Calculate rotation valuee along the Y-axis
+    * Here the multiplier 20 is to
+    * Control the rotation
+    * You can change the value and see the results
+    */
+  const yRotation = 20 * ((xVal - width / 2) / width)
+  
+  /* Calculate the rotation along the X-axis */
+  const xRotation = -20 * ((yVal - height / 2) / height)
+  
+  /* Generate string for CSS transform property */
+  const string = 'perspective(500px) scale(1.1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+  
+  /* Apply the calculated transformation */
+  house.style.transform = string
+}
+
+/* Add listener for mouseout event, remove the rotation */
+house.addEventListener('mouseout', function() {
+    house.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mousedown event, to simulate click */
+house.addEventListener('mousedown', function() {
+    house.style.transform = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mouseup, simulate release of mouse click */
+house.addEventListener('mouseup', function() {
+    house.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
+})
