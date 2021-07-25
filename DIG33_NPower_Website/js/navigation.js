6062e0ad8e8af446f24370ec4bf8ee3a59c39672
s3775633 <s3775633@student.rmit.edu.au>
2021-07-25 19:54:27 +1000

    Code commenting updates
    Added further code comments in navigation.js

--- DIG33_NPower_Website/js/navigation.js
+++ DIG33_NPower_Website/js/navigation.js
@@ -3,22 +3,27 @@ const bsCollapse = new bootstrap.Collapse(menuToggle)
 
 // Function used to close nav menu if in mobile display
 function closeNav() {
+    // Screen width obtained
     var screenWidth = window.innerWidth;
+    // If screen width is less than desktop mode size
     if(screenWidth < 1301)
     {
+        // nav toggled if not already collapsed.
         if(!document.getElementById('toggle').classList.contains('collapsed'))
         {
             bsCollapse.toggle();
         }
     }
-    console.log(screenWidth);
 }
 
 // Function used to display energy-saving incentives page.
 function esPage() {
+    // hidePages function ran to hide all other pages.
     hidePages();
+    // energysaving section and nav obtained
     var esPage = document.querySelector(".energySaving");
     var esNav = document.getElementById("esNav");
+    // energyseving section displayed and nav set to active.
     esPage.style.display = "block";
     esNav.classList.add("active");
 }
@@ -77,11 +82,14 @@ function supportPage() {
 
 // Function used to hide all pages on the site and make all nav options inactive.
 function hidePages() {   
+    // All pages obtained
     var pages = document.getElementsByClassName("page");
+    // For loop used to loop through all pages and hide them
     for(page of pages)
     {
         page.style.display = "none";
     }
+    // Nav links captured and deactivated.
     var navItems = document.getElementsByClassName("nav-link");
     for(navItem of navItems)
     {
