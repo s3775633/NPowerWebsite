4a109986b51c7bd5da6b70219ac0cf0049172599
s3775633 <s3775633@student.rmit.edu.au>
2021-07-26 11:59:50 +1000

    Tile loop effect
    Added tile loop effect for mobile view

--- DIG33_NPower_Website/js/tiles.js
+++ DIG33_NPower_Website/js/tiles.js
@@ -1,29 +1,41 @@
+var tileView = true;
+
 // function used to add and remove classes from home page tiles to give additional
 // hover effects
 function mouseOver(div) {
-    var image = div.querySelector(".image");
-    var text = div.querySelector(".tileText");
-    var heading = div.querySelector(".tileHeading");
-    var tileImage = div.querySelector(".cardImage");
-    var description = div.querySelector(".tileDescription");
-    image.classList.add("fade-out");
-    text.classList.add("fade-out");
-    heading.classList.remove("fade-out-description");
-    description.classList.remove("fade-out-description");
-    tileImage.classList.remove("fade-out-description");
-    image.classList.remove("fade-in");
-    text.classList.remove("fade-in");
-    heading.classList.add("fade-in-description");
-    description.classList.add("fade-in-description");
-    heading.classList.remove("tileDecriptionVisibility");
-    description.classList.remove("tileDecriptionVisibility");
-    tileImage.classList.add("fade-in-description");
-    tileImage.classList.remove("tileDecriptionVisibility");
+  addHover(div);
 }
 
 // function used to add and remove classes from home page tiles to give additional
 // unhover effects
 function mouseOut(div) {
+  removeHover(div);
+}
+
+function addHover(div)
+{
+  var image = div.querySelector(".image");
+  var text = div.querySelector(".tileText");
+  var heading = div.querySelector(".tileHeading");
+  var tileImage = div.querySelector(".cardImage");
+  var description = div.querySelector(".tileDescription");
+  image.classList.add("fade-out");
+  text.classList.add("fade-out");
+  heading.classList.remove("fade-out-description");
+  description.classList.remove("fade-out-description");
+  tileImage.classList.remove("fade-out-description");
+  image.classList.remove("fade-in");
+  text.classList.remove("fade-in");
+  heading.classList.add("fade-in-description");
+  description.classList.add("fade-in-description");
+  heading.classList.remove("tileDecriptionVisibility");
+  description.classList.remove("tileDecriptionVisibility");
+  tileImage.classList.add("fade-in-description");
+  tileImage.classList.remove("tileDecriptionVisibility");
+}
+
+function removeHover(div)
+{
   var image = div.querySelector(".image");
   var text = div.querySelector(".tileText");
   var heading = div.querySelector(".tileHeading");
@@ -40,3 +52,38 @@ function mouseOut(div) {
   description.classList.remove("fade-in-description");
   tileImage.classList.remove("fade-in-description");
 }
+
+var intervalId = window.setInterval(function(){
+  var tiles = document.getElementsByClassName('tile');
+  // Screen width obtained
+  var screenWidth = window.innerWidth;
+  // If screen width is less than desktop mode size
+  if(screenWidth < 576)
+  {
+    if(tileView)
+    {
+      for(tile of tiles)
+      {
+        addHover(tile);
+      }
+      console.log("hover")
+      tileView = false;
+    }
+    else
+    {
+      for(tile of tiles)
+      {
+        removeHover(tile);
+      }
+      tileView = true;
+    }
+  }
+  else
+  {
+    for(tile of tiles)
+    {
+      removeHover(tile);
+    }
+    tileView = true;
+  }
+}, 5000);
