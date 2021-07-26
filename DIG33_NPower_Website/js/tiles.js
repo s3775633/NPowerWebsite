8bf8e5704d2cc4a7d52da88b2b6211cbac6a11f6
s3775633 <s3775633@student.rmit.edu.au>
2021-07-26 13:07:46 +1000

    Mobile Tiles
    Added double tap for mobile tiles.

--- DIG33_NPower_Website/js/tiles.js
+++ DIG33_NPower_Website/js/tiles.js
@@ -1,29 +1,51 @@
 // function used to add and remove classes from home page tiles to give additional
 // hover effects
 function mouseOver(div) {
-    var image = div.querySelector(".image");
-    var text = div.querySelector(".tileText");
-    var heading = div.querySelector(".tileHeading");
-    var tileImage = div.querySelector(".cardImage");
-    var description = div.querySelector(".tileDescription");
-    image.classList.add("fade-out");
-    text.classList.add("fade-out");
-    heading.classList.remove("fade-out-description");
-    description.classList.remove("fade-out-description");
-    tileImage.classList.remove("fade-out-description");
-    image.classList.remove("fade-in");
-    text.classList.remove("fade-in");
-    heading.classList.add("fade-in-description");
-    description.classList.add("fade-in-description");
-    heading.classList.remove("tileDecriptionVisibility");
-    description.classList.remove("tileDecriptionVisibility");
-    tileImage.classList.add("fade-in-description");
-    tileImage.classList.remove("tileDecriptionVisibility");
+  // Screen width obtained
+  var screenWidth = window.innerWidth;
+  // If screen width is less than desktop mode size
+  if(!(screenWidth < 576))
+  {
+    addHover(div);
+  }
 }
 
 // function used to add and remove classes from home page tiles to give additional
 // unhover effects
 function mouseOut(div) {
+  // Screen width obtained
+  var screenWidth = window.innerWidth;
+  // If screen width is less than desktop mode size
+  if(!(screenWidth < 576))
+  {
+    removeHover(div);
+  }
+}
+
+function addHover(div)
+{
+  var image = div.querySelector(".image");
+  var text = div.querySelector(".tileText");
+  var heading = div.querySelector(".tileHeading");
+  var tileImage = div.querySelector(".cardImage");
+  var description = div.querySelector(".tileDescription");
+  image.classList.add("fade-out");
+  text.classList.add("fade-out");
+  heading.classList.remove("fade-out-description");
+  description.classList.remove("fade-out-description");
+  tileImage.classList.remove("fade-out-description");
+  image.classList.remove("fade-in");
+  text.classList.remove("fade-in");
+  heading.classList.add("fade-in-description");
+  description.classList.add("fade-in-description");
+  heading.classList.remove("tileDecriptionVisibility");
+  description.classList.remove("tileDecriptionVisibility");
+  tileImage.classList.add("fade-in-description");
+  tileImage.classList.remove("tileDecriptionVisibility");
+}
+
+function removeHover(div)
+{
   var image = div.querySelector(".image");
   var text = div.querySelector(".tileText");
   var heading = div.querySelector(".tileHeading");
@@ -39,4 +61,53 @@ function mouseOut(div) {
   heading.classList.remove("fade-in-description");
   description.classList.remove("fade-in-description");
   tileImage.classList.remove("fade-in-description");
+}
+
+function tileClick(div)
+{
+  var description = div.querySelector(".tileDescription");
+  // Screen width obtained
+  var screenWidth = window.innerWidth;
+  // If screen width is less than desktop mode size
+  if(screenWidth < 576)
+  {
+    if(description.classList.contains("fade-in-description"))
+    {
+      navigate(div);
+      resetTiles();
+    }
+    else
+    {
+      addHover(div);
+    }
+  }
+  else
+  {
+    navigate(div)
+  }
+}
+
+function navigate(div)
+{
+  if(div.classList.contains("reduceCost"))
+  {
+    esPage();
+  }
+  else if(div.classList.contains("alternatives"))
+  {
+    impPage();
+  }
+  else if(div.classList.contains("environment"))
+  {
+    prodPage();
+  }
+}
+
+function resetTiles()
+{
+  var tiles = document.getElementsByClassName('tile');
+  for(tile of tiles)
+  {
+    removeHover(tile);
+  }
 }
